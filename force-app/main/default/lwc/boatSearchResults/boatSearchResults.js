a92676b74f98ea220142e8f8e01be774fe5c7f63
OlegBogdanovych <bogdanovych.oleg@gmail.com>
2021-02-02 11:36:26 +0200

    Task 5, Task 6

---
+++ force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -0,0 +1,72 @@
+/**
+ * Created by olehbohdanovych on 02.02.2021.
+ */
+
+import { LightningElement, wire } from "lwc";
+
+import getBoats from "@salesforce/apex/BoatDataService.getBoats";
+
+import {
+  subscribe,
+  unsubscribe,
+  APPLICATION_SCOPE,
+  MessageContext
+} from 'lightning/messageService';
+
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+
+const SUCCESS_TITLE = 'Success';
+const MESSAGE_SHIP_IT = 'Ship it!';
+const SUCCESS_VARIANT = 'success';
+const ERROR_TITLE   = 'Error';
+const ERROR_VARIANT = 'error';
+
+export default class BoatSearchResults extends LightningElement {
+  selectedBoatId;
+  columns = [];
+  boatTypeId = '';
+  boats;
+  isLoading = false;
+
+  // wired message context
+  @wire(MessageContext)
+  messageContext;
+  // wired getBoats method
+  @wire(getBoats, { boatTypeId: '$boatTypeId' })
+  wiredBoats(result) {
+    this.boats = result;
+  }
+
+  // public function that updates the existing boatTypeId property
+  // uses notifyLoading
+  searchBoats(boatTypeId) { }
+
+  // this public function must refresh the boats asynchronously
+  // uses notifyLoading
+  refresh() { }
+
+  // this function must update selectedBoatId and call sendMessageService
+  updateSelectedTile() { }
+
+  // Publishes the selected boat Id on the BoatMC.
+  sendMessageService(boatId) {
+    // explicitly pass boatId to the parameter recordId
+  }
+
+  // The handleSave method must save the changes in the Boat Editor
+  // passing the updated fields from draftValues to the
+  // Apex method updateBoatList(Object data).
+  // Show a toast message with the title
+  // clear lightning-datatable draft values
+  handleSave(event) {
+    // notify loading
+    const updatedFields = event.detail.draftValues;
+    // Update the records via Apex
+    updateBoatList({data: updatedFields})
+      .then(() => {})
+      .catch(error => {})
+      .finally(() => {});
+  }
+  // Check the current value of isLoading before dispatching the doneloading or loading custom event
+  notifyLoading(isLoading) { }
+}
